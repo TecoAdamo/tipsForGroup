8e242e83927e09cc5152848ebe4c32172b93def2
Mateus <mateus@MacBook-Air-de-Fix.local>
2025-04-02 22:32:32 -0300

    Feat: first app with SwiftUI

---
+++ MyAppSwiftUi/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  MyAppSwiftUi
+//
+//  Created by Mateus on 02/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State  private var checkAmount = 0.0
+    @State private  var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
+    var body: some View {
+      
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                        .keyboardType(.decimalPad)
+                }
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                }
+                }
+            
+        }
+    }
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ MyAppSwiftUi/MyAppSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppSwiftUiApp.swift
+//  MyAppSwiftUi
+//
+//  Created by Mateus on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAppSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
