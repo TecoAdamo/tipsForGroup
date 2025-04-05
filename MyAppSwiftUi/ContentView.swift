5c72ff61c489eebf7f416125c0b60ed6f937367b
Mateus <mateus@MacBook-Air-de-Fix.local>
2025-04-05 02:11:00 -0300

    feat: app created

--- MyAppSwiftUi/ContentView.swift
+++ MyAppSwiftUi/ContentView.swift
@@ -8,30 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State  private var checkAmount = 0.0
+    @State  private var checkAmountText = ""
     @State private  var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocus: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    var totaoPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandeTotal = checkAmount + tipValue
+        let amountPerPerson = grandeTotal / peopleCount
+        return amountPerPerson
+    }
     
+    
+    var checkAmount: Double {
+        Double(checkAmountText.replacingOccurrences(of: ",", with: ".")) ?? 0.0
+    }
     
     var body: some View {
-      
+        NavigationView{
             Form{
                 Section{
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                    //Textfield (input) value for numbers, and text for strings
+                    TextField("Informe o valor: ", text: $checkAmountText)
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocus)
+                    Picker("Total de pessoas na mesa: ", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) Pessoas")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
                 }
                 Section{
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                    
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Porcentagem: ")
                 }
+                Section{
+                    Text(totaoPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                } header: {
+                    Text("Total por pessoa:")
                 }
-            
+            }
+            .navigationTitle("MonTips")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Concluído"){
+                        amountIsFocus = false
+                    }
+                }
+            }
         }
     }
-
-
-
+    
+    
+    
+}
 #Preview {
     ContentView()
 }
